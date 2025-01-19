af4c90fcb9178aaef68b9caae89c92f733c59629
derek williams <derek61@gmail.com>
2025-01-19 04:47:42 -0700

    simple Next app  with mock data working

---
+++ data/articles.ts
@@ -0,0 +1,21 @@
+// data/articles.ts
+export const articles = [
+  {
+    title: "Article 1",
+    description: "This is the first article",
+    url: "http://localhost:3000/articles/1",
+    date: new Date(),
+  },
+  {
+    title: "Article 2",
+    description: "This is the second article",
+    url: "http://localhost:3000/articles/2",
+    date: new Date(),
+  },
+  {
+    title: "Article 3",
+    description: "This is the third article",
+    url: "http://localhost:3000/articles/3",
+    date: new Date(),
+  },
+];
