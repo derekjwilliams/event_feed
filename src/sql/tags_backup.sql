bafdc6167c61197e43835958e4eccfc79f990d0d
derek williams <derek61@gmail.com>
2025-01-30 13:54:26 -0700

    add sql backups

---
+++ src/sql/tags_backup.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.tags ("name") VALUES
+	 ('Housing'),
+	 ('Willamette'),
+	 ('PNCA'),
+	 ('Putnam'),
+	 ('Salem'),
+	 ('Activities'),
+	 ('Waller Hall'),
+	 ('Salem Campus'),
+	 ('Ford Hall'),
+	 ('Mary Stuart Rogers Music Center');
+INSERT INTO public.tags ("name") VALUES
+	 ('Truman Wesley Collins Legal Center'),
+	 ('Arlene and Harold Schnitzer Center for Art and Design'),
+	 ('PNCA Campus');
