b4634c9aaa14af976ff38359d57e00c9270e2f3e
derek williams <derek61@gmail.com>
2025-02-01 23:07:40 -0700

    some improvements to CLS score

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,7 +2,7 @@ import SuspendedEventsList from '@/components/EventList'
 
 export default function EventsPage() {
   return (
-    <main className="p-8 m-2">
+    <main className="p-4 m-2">
       <h1 className="text-2xl font-bold mb-4">Events</h1>
       <SuspendedEventsList />
     </main>
