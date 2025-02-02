309757a5c5566fd23459e643c6fa4a7b21e59583
derek williams <derek61@gmail.com>
2025-02-01 21:50:04 -0700

    configured caching

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,9 +3,19 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode } from 'react'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+const MINUTE = 1000 * 60
 export default function Providers({ children }: { children: ReactNode }) {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // Do not refetch data once it's loaded
+        staleTime: 2 * MINUTE, // Data is considered fresh for 4 minutes
+        gcTime: 10 * MINUTE, // Cached data remains in memory forever (or until manually cleared)
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+      },
+    },
+  })
 
   return (
     <QueryClientProvider client={queryClient}>
