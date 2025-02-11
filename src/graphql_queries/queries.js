f4f16a256df80b45284a9ee64bf14e7be1f36613
derek williams <derek61@gmail.com>
2025-02-11 15:29:46 -0700

    update query for hasura, including cursor, orderBy, and empty string tag example

--- src/graphql_queries/queries.js
+++ src/graphql_queries/queries.js
@@ -1,26 +1,40 @@
 export const EVENTS_QUERY = `
-  query getEventsByDateAndTags($tagNames: _text, $pubDate: String!) {
-  getEventsByDateAndTags(
-    limit: 10
-    args: {
-      p_tag_names: $tagNames,
-      p_pub_date: $pubDate
-    }
+
+query GetEventsByDateAndTags($tagNames: _text, $pubDate: String!) {
+  getEventsByDateAndTags_connection(
+    first: 10
+    orderBy: {eventStartDate: ASC}
+    args: {p_tag_names: $tagNames, p_pub_date: $pubDate}
   ) {
-    title
-    description
-    eventStartDate
-    eventEndDate
-    location
-    geoLocation
-    eventTagsAsString
-    eventTags {
-      tag {
-        name
+    edges {
+      node {
+        title
+        description
+        eventStartDate
+        eventEndDate
+        location
+        geoLocation
+        eventTags {
+          tag {
+            name
+          }
+        }
       }
     }
   }
-}`
+}
+`
+
+// {
+//   "tagNames": "{Salem Campus}", // YES THIS IS WEIRD, hasura has a _text type here, not a [String] which is what Postgrapile uses, see https://www.postgresql.org/docs/current/arrays.html#ARRAYS-INPUT
+//   "pubDate": "2017-01-01"
+// }
+
+// For tags with empty string: ''
+// {
+//   "tagNames": "{ \"\" }",
+//   "pubDate": "2017-01-01"
+// }
 
 export const TAGS_QUERY = `
   query {
